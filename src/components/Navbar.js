b325018be73943bf953533e8998d583665dec1d1
Vishnu Priya <89837239+VishnupriyaThammina@users.noreply.github.com>
2024-07-06 13:23:03 +0530

    homepage ui upadates, finished navbar, upadted css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,35 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import ArticleRoundedIcon from '@mui/icons-material/ArticleRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import img1 from '../Assets/images/blogg-no-bg.png';
 function Navbar() {
   return (
   <>
   <Grid container className='n-wrap'>
 <Grid item className='n-w-1 '>
-  <Grid container>
+  <Grid container className='nw-b-1'>
 <Grid item  className='nw1'>
-<Typography variant='h6'>Blogg</Typography>
+  
+<p className='logo-p'>Blogg</p>
 </Grid> 
 
 <Grid item  className='nw1-1'>
-<Typography variant='h6'>Feed</Typography>
+<ArticleRoundedIcon  fontSize='small'/>
+
 </Grid> 
 
 <Grid item  className='nw1-1'>
-<AddBoxOutlinedIcon/>
+<BorderColorRoundedIcon fontSize='small'/>
 
 </Grid> 
 <Grid item  className='nw1-1'>
-  <AccountCircleRoundedIcon/>
+  <AccountCircleRoundedIcon  fontSize='small'/>
+</Grid> 
+<Grid item  className='nw1-1'>
+  <LogoutRoundedIcon  fontSize='small'/>
 </Grid> 
  </Grid>
 </Grid>
--- src/pages/blog-pages/HomePage.js
+++ src/pages/blog-pages/HomePage.js
@@ -1,11 +1,56 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import { Grid, Typography } from '@mui/material'
+import img1 from '../../Assets/images/articles.jpg';
+import PostCard from '../../components/PostCard';
 function HomePage() {
   return (
-    <div>
+    <>
 
         <Navbar/>
-    </div>
+        {/* banner image in home page ui */}
+<Grid conatiner className='wrap-1-h'>
+<Grid item className='wrap-hp'>
+<Grid container item className='banner-img-wrap'>
+<Grid  item className='bn-t'>
+  <Grid container className='bn-t-1'>
+
+  <Grid item container className='display-text-1' >
+    <h3>Share your knowledge, Memories, Experiences, Just Blog it </h3>
+  </Grid>
+ 
+  <Grid item container className='display-btn-1' >
+   <button className='btn-white'>Create Blog</button> </Grid>
+  </Grid>
+  </Grid>
+
+</Grid>
+
+</Grid>
+</Grid>
+
+{/* top 5 posts  */}
+    
+    <Grid container className='wrap-1-h'>
+      <Grid item className='wrap-hp'>
+     <Grid container className='wrap-2-h'>
+<Grid container className='text-1-h4'>
+  <Typography variant='h4' sx={{fontWeight:700}}>
+    Recent posts
+  </Typography>
+</Grid>
+        <Grid container >
+        <PostCard/>
+
+        </Grid>
+        <Grid container>
+        <PostCard/>
+      
+        </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    </>
   )
 }
 
